ae9abebbcbac55c2e58819332ada46d4de1a2202
root <root@shawnqiang-ubuntu.(none)>
2012-03-07 14:43:26 +0800

    update uh_sql.sql

--- docs/uh_sql.sql
+++ docs/uh_sql.sql
@@ -1,3 +1,3 @@
-ALTER TABLE oc_product_description ADD COLUMN desc_summary TEXT not null COMMENT 'product summary description';
-ALTER TABLE oc_product_description ADD COLUMN desc_function TEXT not null COMMENT 'product functional description';
-ALTER TABLE oc_product_description ADD COLUMN desc_style TEXT not null COMMENT 'product style description';
+ALTER TABLE oc_product_description ADD COLUMN summary_desc TEXT not null COMMENT 'product summary description';
+ALTER TABLE oc_product_description ADD COLUMN function_desc TEXT not null COMMENT 'product functional description';
+ALTER TABLE oc_product_description ADD COLUMN style_desc TEXT not null COMMENT 'product style description';
